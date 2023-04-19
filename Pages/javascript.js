3ae67b34eb713a55b9640f25276373f57711d544
Edisavdagic <119938495+Edisavdagic@users.noreply.github.com>
2023-04-19 14:25:18 +0200

    Javascript edis

--- Pages/javascript.js
+++ Pages/javascript.js
@@ -1,5 +1,48 @@
-function changeImage()
-{ 
- var img = document.getElementById('hej');
- img.src= "../Images/Arrangementer/rundbold.jpeg";
-}
+var news = [
+    { title: "UCL søger nye personalemedlemmer til Årsfesten ", content: "Er godt humør, fælleskab og planlægning noget du brænder for? Så meld dig ind i Festudvalget. Klik her." },
+    { title: "Fredagsbaren", content: "Tilbud på alt drikkelse til fredagsbaren ved fremvisning af brugeroprettelse." },
+    { title: "En god nyhed for dig som studerende", content: "Der er en masse fordele på vej til dig. Stuierabat, resscourser og meget mere. Klik her for at læse mere." },
+  ];
+  
+  var currentNewsIndex = 0; // Variabel til at holde styr på den aktuelle nyhed
+  var button = document.querySelector(".newsletter-button");
+  var isActive = false; // Variabel til at holde styr på om knappen er aktiveret eller ej
+  
+  // Function to get next news item
+  function getNextNews() {
+    var newsItem = news[currentNewsIndex];
+    currentNewsIndex = (currentNewsIndex + 1) % news.length;
+    return newsItem;
+  }
+  
+  // Add click event listener to the button
+  button.addEventListener("click", toggleNewsletter);
+  
+  // Function to toggle newsletter
+  function toggleNewsletter() {
+    isActive = !isActive; // Toggle boolean value
+  
+    if (isActive) {
+      generateNews();
+    } else {
+      document.querySelector(".newsletter").innerHTML = "";
+    }
+  }
+  
+  // Function to generate news
+  function generateNews() {
+    var newsletter = document.querySelector(".newsletter");
+    var newsContent = "";
+  
+    // Generer nyheder i rækkefølge, så titel og indhold passer sammen
+    for (var i = 0; i < 3; i++) {
+      var newsItem = getNextNews();
+      newsContent += `<h2>${newsItem.title}</h2>`;
+      newsContent += `<p>${newsItem.content}</p>`;
+    }
+  
+    newsletter.innerHTML = newsContent;
+  }
+  
+  
+  
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,32 @@
-/*<a href="javascript:void(0);" onclick="burgerMenu()">
-    <img src="Images/Header/burger-hvid.png" class="ikon" alt="Burger-menu">
-</a>
 
-//BURGER
-// Fundet på W3Schools: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
-function burgerMenu() {
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+// Get the modal element
+const modal = document.getElementById('myModal');
 
-  */
+// Get the button that opens the modal
+const modalButton = document.getElementById('modalButton');
 
+// Get the span element that closes the modal
+const closeSpan = modal.querySelector('.close');
 
-  function changeimage()
-{ 
- var img = document.getElementById('hej');
- img.src= '../Images/Arrangementer/rundbold.jpeg';
+// Function to open the modal
+function openModal() {
+  modal.style.display = 'block';
 }
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Event listener for button click to open the modal
+modalButton.addEventListener('click', openModal);
+
+// Event listener for span click to close the modal
+closeSpan.addEventListener('click', closeModal);
+
+// Event listener for outside click to close the modal
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
